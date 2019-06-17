0d03cf4269efc9ad5cde141c33b007528e74df67
MattDobsonWeb <hello@mattdobson.me>
2019-06-18 00:45:06 +0100

    Finally got images working correctly in posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,109 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
+import { Link, graphql, StaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import Layout from '../components/Layout';
 
-export default ({ data }) => {
-  console.log(data);
-  return <Layout>Hello World</Layout>;
+class BlogRoll extends React.Component {
+  render() {
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
+
+    return (
+      <Layout>
+        <div className="container">
+          <div className="row">
+            {posts &&
+              posts.map(({ node: post }) => (
+                <div className="col-md-4" key={post.id}>
+                  <article
+                    className={`blog-list-item tile is-child box notification ${
+                      post.frontmatter.featuredpost ? 'is-featured' : ''
+                    }`}
+                  >
+                    <header>
+                      {post.frontmatter.featuredimage ? (
+                        <div className="featured-thumbnail">
+                          <Img
+                            fluid={
+                              post.frontmatter.featuredimage.childImageSharp
+                                .fluid
+                            }
+                          />
+                        </div>
+                      ) : null}
+                      <p className="post-meta">
+                        <Link
+                          className="title has-text-primary is-size-4"
+                          to={post.fields.slug}
+                        >
+                          {post.frontmatter.title}
+                        </Link>
+                        <span> &bull; </span>
+                        <span className="subtitle is-size-5 is-block">
+                          {post.frontmatter.date}
+                        </span>
+                      </p>
+                    </header>
+                    <p>
+                      {post.excerpt}
+                      <br />
+                      <br />
+                      <Link className="button" to={post.fields.slug}>
+                        Keep Reading →
+                      </Link>
+                    </p>
+                  </article>
+                </div>
+              ))}
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+BlogRoll.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array
+    })
+  })
 };
 
-export const query = graphql`
-  query BlogRollQuery {
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-            featuredpost
-            featuredimage {
-              childImageSharp {
-                fluid(maxWidth: 120, quality: 100) {
-                  ...GatsbyImageSharpFluid
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query BlogRollQuery {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 400)
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                templateKey
+                date(formatString: "MMMM DD, YYYY")
+                featuredpost
+                featuredimage {
+                  childImageSharp {
+                    fluid(maxWidth: 120, quality: 100) {
+                      ...GatsbyImageSharpFluid
+                    }
+                  }
                 }
               }
             }
           }
         }
       }
-    }
-  }
-`;
+    `}
+    render={(data, count) => <BlogRoll data={data} count={count} />}
+  />
+);
